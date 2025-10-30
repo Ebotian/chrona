66d3b6a95902f5e1da5581cdda5f239c67df280c
EbotianArch <ebotian8964@gmail.com>
2025-10-30 10:59:46 +0800

    清空报错

--- packages/motion/playground/src/main.tsx
+++ packages/motion/playground/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App.js';
 import './styles.css';
 
 const container = document.getElementById('root');
